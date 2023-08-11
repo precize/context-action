0c703f7663f3ba823b7cdd189f8ffe645570c70d
abhayanoop <abhay@precize.io>
2023-08-11 20:58:41 +0530

    Debug logs

--- context.sh
+++ context.sh
@@ -10,6 +10,9 @@ do
    echo "In progress: $inprogress"
    nostatus=$(gh run list --repo $GITHUB_REPOSITORY --json headSha,status --jq '.[] | select(.headSha=="$GITHUB_SHA")')
    echo "No status: $nostatus"
+   grepway=$(gh run list --status='in_progress' --repo $GITHUB_REPOSITORY --json headSha | grep $GITHUB_SHA)
+   echo "Grep: $grepway"
+   echo "Final: gh run list --status='in_progress' --repo $GITHUB_REPOSITORY --json headSha --jq '.[] | select(.headSha=="$GITHUB_SHA")')"
    running=$(gh run list --status='in_progress' --repo $GITHUB_REPOSITORY --json headSha --jq '.[] | select(.headSha=="$GITHUB_SHA")')
    echo "Running: $running"
 

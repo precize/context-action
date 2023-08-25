0060ab86346a27396e640310404c3a31a23e86e9
abhayanoop <abhay@precize.io>
2023-08-25 17:10:19 +0530

    Tag git_file to terraform resources

--- context.sh
+++ context.sh
@@ -11,7 +11,7 @@ do
 done
 
 wget -q -O - https://github.com/bridgecrewio/yor/releases/download/0.1.183/yor_0.1.183_linux_amd64.tar.gz | tar -xvz -C /tmp
-/tmp/yor tag -d . --tag-groups git --skip-tags git_file,git_org,git_modifiers,git_last_modified_by,git_last_modified_at --parsers Terraform --tag-prefix precize_ --tag-local-modules false
+/tmp/yor tag -d . --tag-groups git --skip-tags git_org,git_modifiers,git_last_modified_by,git_last_modified_at --parsers Terraform --tag-prefix precize_ --tag-local-modules false
 
 git config --global user.name 'Precize'
 git config --global user.email 'noreply@precize.ai'
